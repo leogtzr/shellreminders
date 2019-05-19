8fa1bb62086cf4751d052400f868b750b943521c
leogtzr <leogutierrezramirez@gmail.com>
2019-05-19 15:38:56 -0600

    fix test (undefined type)

--- shellreminder_test.go
+++ shellreminder_test.go
@@ -34,13 +34,12 @@ func generateBaseDateTime() time.Time {
 
 func TestReminderRecordParsing(t *testing.T) {
 	tests := []struct {
-		input                string
-		expectedDays         int
-		expectedName         string
-		expectedReminderType ReminderType
+		input        string
+		expectedDays int
+		expectedName string
 	}{
-		{`Santander Platino;18`, 18, "Santander Platino", RecurrentReminder},
-		{`Promotions;13;counter`, 13, "Promotions", Counter},
+		{`Santander Platino;18`, 18, "Santander Platino"},
+		{`Promotions;13;counter`, 13, "Promotions"},
 	}
 
 	for _, tt := range tests {
@@ -53,9 +52,6 @@ func TestReminderRecordParsing(t *testing.T) {
 		}
 		if reminder.EveryWhen != tt.expectedDays {
 			t.Fatalf("got=%d as days, expected=%d", reminder.EveryWhen, tt.expectedDays)
-		}
-		if reminder.Type != tt.expectedReminderType {
-			t.Fatalf("got=%+v as type, expected=%+v", reminder.Type, tt.expectedReminderType)
 		}
 	}
 }
