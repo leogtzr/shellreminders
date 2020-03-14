acf6cf98953753b806f6b424dd81473bdc3d29b3
leogtzr <leogutierrezramirez@gmail.com>
2020-03-14 15:09:02 -0700

    use constants

--- shellreminder.go
+++ shellreminder.go
@@ -29,6 +29,9 @@ const (
 	lessThanDays               = 8
 	recordFileSeparator        = ";"
 	warningRemainingDays       = 2
+	redHexColor                = "#E88388"
+	yellowHexColor             = "#DBAB79"
+	greenHexColor              = "#A8CC8C"
 )
 
 var cmdArgs = [6]string{"-f", "smblock", "-w", "900", "-F", "border"}
