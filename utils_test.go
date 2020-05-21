987e535ba83eb48221a8d70c58ba061bbd0c3573
leogtzr <leogutierrezramirez@gmail.com>
2020-05-21 10:51:58 -0600

    add unit test for buildHash function

--- utils_test.go
+++ utils_test.go
@@ -22,3 +22,27 @@ func Test_colorForMessages(t *testing.T) {
 		}
 	}
 }
+
+func Test_buildHash(t *testing.T) {
+	type test struct {
+		reminderName string
+		want         string
+	}
+
+	tests := []test{
+		test{
+			reminderName: "hola",
+			want:         "77a69f8b593ff373140666fe1faddcb7",
+		},
+		test{
+			reminderName: "test1",
+			want:         "0ebec8ca1bbe2cf1922446edbf9eb9ba",
+		},
+	}
+
+	for _, tt := range tests {
+		if got := buildHash(tt.reminderName); got != tt.want {
+			t.Errorf("got=[%s], expected=[%s]", got, tt.want)
+		}
+	}
+}
